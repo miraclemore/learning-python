e1aa6d05bb5cbf1ccb1c64978b5e6f342f06ec6e
miracle.huang <miraclemore.huang@gmail.com>
2020-09-23 23:28:27 +0800

    Update demo1
    Signed-off-by: miracle.huang <miraclemore.huang@gmail.com>

---
+++ demo1.py
@@ -0,0 +1,12 @@
+print('helloworld')
+print("helloworld")
+
+print("python", "c++", "go")
+
+print(520)
+print(3+1)
+print(98.5)
+
+fp = open("D:/test.txt", 'a+')
+print('hello python', file=fp)
+fp.close()
